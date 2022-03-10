ba0b8f32c9b435af91d32496d7c73374ba909e06
sshubh22 <100853456+sshubh22@users.noreply.github.com>
2022-03-10 20:45:59 +0530

    Add files via upload

---
+++ pat2.java
@@ -0,0 +1,18 @@
+ class pat2  
+	{              
+		public static void main(String[] args)  
+		{  
+			int a = 65; 
+
+				for (int i = 0; i <= 4; i++)  
+				{  
+     
+					for (int j = 0; j <= i; j++)  
+					{  
+  
+						System.out.print((char) (a + j) + " ");   
+					}  
+					System.out.println();  
+				}  
+		}  
+	}  
